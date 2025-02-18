6820b117101327281965ef2e8b9631099bce170f
Jeffer52 <yegarces@estudiante.uniajc.edu.co>
2025-02-18 13:02:02 +0000

    //Ejercicio 10: sumas de opuestos

--- Clase1/Clase2/Arreglo.java
+++ Clase1/Clase2/Arreglo.java
@@ -149,4 +149,20 @@ System.out.println(" Ganancias de vivienda" + (i+1)+ " : " + ganancias);
         System.out.println("El numero " + incognita + " aparece" + contar + " veces. ");
     }
  }
+ //Ejercicio 10: sumas de opuestos 
+ public Ejercicio10 {
+    public static void main(String[]arg){
+        int[] A ={8,5,3,10,2,8,1};
+        int [] B = new int[(A.length + 1)/2];
+        //Recorremos el arreglo B para calcular las sumas de los opuestos }
+        for(int i =0; i<B.length; i++){
+            B[i]=A[i] + A[A.length - 1 - i];
+            System.out.print("Arreglo resultante: ");
+            for(int numero : B){
+                System.out.println(numero + "");
+            }
+        }
+    }
+ }
+
 
