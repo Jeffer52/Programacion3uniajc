9fdd1950b57133e24affce6f7b7d2d291a073542
Jeffer52 <yegarces@estudiante.uniajc.edu.co>
2025-02-18 12:52:37 +0000

    Ejercicio 1 suma de los N numeros

---
+++ Clase1/Clase2/Arreglo.java
@@ -0,0 +1,11 @@
+// Ejercicio 1 suma de los N numeros 
+public class Ejercicio{
+    public static void main(String[]arg){
+        int[] numero = {1,2,3,4,5}
+        int suma= 0; // Variable para almacenar la suma de los elementos del arreglo
+        for (int numero : numero){ // Blucle que recorre cada numero en el arreglo 
+            suma += numeros //Acomulamos la suma  de los arreglos 
+        }
+        System.out.println("Suma total: "+ suma)
+    }
+}
