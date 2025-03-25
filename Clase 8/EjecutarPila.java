98e3cf6f453b5107458e1689d0cf657c18895283
Jeffer52 <yegarces@estudiante.uniajc.edu.co>
2025-03-25 15:48:20 +0000

    clase 8 pila

---
+++ Clase 8/EjecutarPila.java
@@ -0,0 +1,23 @@
+import java.util.Stack;
+
+public class EjecutarPila {
+        public static void main(String[]args){
+
+            //Creacion de estructura Stack (Pila)
+            Stack<String> nombre = new Stack <>();
+
+            System.out.println(nombres.empty()); //true
+
+            //insertar elementos en la pila
+           nombres.push(item:"Pedro");
+           nombres.push(item:"Juan");
+           nombres.push(item:"Maria");
+           nombres.push(item:"Camila");
+           nombres.push(item:"Daniela");
+
+           //Imprimir la pila
+           System.out.println(nombres);
+
+
+        }
+    }
