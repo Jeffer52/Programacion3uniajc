5d54a5b661d3adf7e749fc8676b163b218e04cc8
Jeffer52 <yegarces@estudiante.uniajc.edu.co>
2025-04-01 16:44:45 +0000

    Creacion de la clase ejecutar

---
+++ Clase9/EjecutarPilaTrabajador.java
@@ -0,0 +1,11 @@
+public class EjecutarPilaTrabajador{
+    public static void main(String[]args){
+
+        StackTrabajador> t= new Stack<>();
+
+        Trabajador t1 = new Trabajador(cedula "1234",nombre:"Juan", salario: 2200.0);
+        Trabajador t2 = new Trabajador(cedula "4321",nombre:"Pedro", salario: 3200.0);
+        Trabajador t3 = new Trabajador(cedula "3241",nombre:"Luis", salario: 4200.0);
+     
+    }  
+}
