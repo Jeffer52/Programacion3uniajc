f9a680c44b90c764a97a29d463f1ec09f1f8fedc
Jeffer52 <yegarces@estudiante.uniajc.edu.co>
2025-04-01 16:13:40 +0000

    Creacion de la carpeta 9 y la clase trabajador

--- Clase9/Trabajdor.java
+++ Clase9/Trabajdor.java
@@ -1,4 +1,11 @@
 public class Trabajador{
+private String cedula;
+private String nombre;
+private double salario;
 
-    public static void
+public Trabajador (String cedula, String nombre, double salario){
+    this.cedula = cedula;
+    this.nombre = nombre;
+    this.salario= salario;
+}
 }
