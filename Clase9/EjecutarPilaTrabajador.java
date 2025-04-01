a37ae114dd492789ddd6bc82df3fbb02c2834de6
Jeffer52 <yegarces@estudiante.uniajc.edu.co>
2025-04-01 17:04:37 +0000

    Se agregp el metodo elemetAt para recorrer la pila de trabajadores

--- Clase9/EjecutarPilaTrabajador.java
+++ Clase9/EjecutarPilaTrabajador.java
@@ -6,6 +6,18 @@ public class EjecutarPilaTrabajador{
         Trabajador t1 = new Trabajador(cedula "1234",nombre:"Juan", salario: 2200.0);
         Trabajador t2 = new Trabajador(cedula "4321",nombre:"Pedro", salario: 3200.0);
         Trabajador t3 = new Trabajador(cedula "3241",nombre:"Luis", salario: 4200.0);
-     
+        
+        trabajadores.push(t1);
+        trabajadores.push(t2);
+        trabajadores.push(t3);
+
+        //System.out.println(trabajadores);
+
+       System.out.println(trabajadores.size());
+
+       for(int 1 = 0; 1< trabajadores.size(); 1++){
+         System.out.println(trajadores.elementAt(i).getNombre());
+       }
+
     }  
 }
